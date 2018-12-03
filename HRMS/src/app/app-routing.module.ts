4e3eae6529e7a9a50e939ff1f1732bed30a35517
vishalsehgalsofbang <45191802+vishalsehgalsofbang@users.noreply.github.com>
2018-12-03 16:17:15 +0530

    new

--- HRMS/src/app/app-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Route, RouterModule } from '@angular/router';
-
-@NgModule({
-  imports: [RouterModule,CommonModule,],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
--- HRMS/src/app/hero.ts
+++
@@ -1,8 +0,0 @@
-export class Hero {
-    constructor(
-    public id: number,
-    public email: string,
-    public password: string,
-    public alterEgo?: string
-  ) {  }
-}
